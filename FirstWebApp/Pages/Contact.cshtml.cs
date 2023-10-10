163809c614ff2685e509e3e3ee7a8e2367d671f6
Yohan Shimalka <yohanshimalka1999@gmail.com>
2023-10-10 14:52:23 +0530

    1st Commit

---
+++ FirstWebApp/Pages/Contact.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstWebApp.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public bool hasData = false;
+        public string firstname = "";
+        public string lastname = "";
+        public string message = "";
+        public void OnGet()
+        {
+        }
+
+		public void OnPost()
+		{
+            hasData = true;
+            firstname = Request.Form["firstname"];
+            lastname = Request.Form["lastname"];
+            message = Request.Form["message"];
+		}
+
+	}
+}
